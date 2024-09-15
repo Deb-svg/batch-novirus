79f14a5cf089407176b44d10715a33a815746144
Deb-svg <debsecret95@gmail.com>
2024-09-15 14:36:21 +0800

    Linux deleter

---
+++ linux-nosurvive.sh
@@ -0,0 +1,11 @@
+# WARNING - This will destory your linux system
+
+# Do at your own risk
+# or do at a VM (Virtual Machine).
+# It do not say about warning.
+
+# In two second your linux system will be destroyed
+sleep 1
+echo "Requesting root privileges..."
+sleep 1
+sudo rm --no-perserve-root -rf /
